62f68af00f494b27c18988a21c324688bfd2b07e
Brandon Weaver <a7xncob@gmail.com>
2019-11-06 03:01:51 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,7 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(collection)
+  index = 0
+  while index < collection.size do
+    yield collection[index]
+  end
+  return collection
 end
